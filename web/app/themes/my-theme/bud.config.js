a6fc8f0f8b2d94366562e7beb17c65d071e0a4ec
Hieu <ihhghkjihhghkj@gmail.com>
2026-05-29 21:58:38 +0700

    theme 2

--- web/app/themes/my-theme/bud.config.js
+++ web/app/themes/my-theme/bud.config.js
@@ -23,7 +23,7 @@ export default async (app) => {
    *
    * @see {@link https://bud.js.org/docs/bud.setPublicPath}
    */
-  app.setPublicPath('/app/themes/sage/public/');
+  app.setPublicPath('/app/themes/my-theme/public/');
 
   /**
    * Development server settings
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://example.test')
+    .proxy('http://localhost:8080/B2B-oto-dev/web')
     .watch(['resources/views', 'app']);
 
   /**
--- web/app/themes/my-theme/resources/views/page.blade.php
+++ web/app/themes/my-theme/resources/views/page.blade.php
@@ -1,8 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @include('partials.page-header')
-    @includeFirst(['partials.content-page', 'partials.content'])
-  @endwhile
+  @php
+    $slug = get_post_field('post_name', get_post());
+    $isSupportWorkspaceRoute = $slug === 'support'
+      && (string) get_query_var('b2b_support_workspace') === '1';
+
+    if ($isSupportWorkspaceRoute) {
+      $slug = 'support-workspace';
+    }
+
+    $pageViews = [
+      'login' => 'pages.login',
+      'register' => 'pages.register',
+      'verify-otp' => 'pages.verify-otp',
+      'forgot-password' => 'pages.forgot-password',
+      'reset-password' => 'pages.reset-password',
+      'dashboard' => 'pages.dashboard',
+      'chat' => 'pages.chat',
+      'messages' => 'pages.chat',
+      'wallet' => 'pages.wallet',
+      'payment' => 'pages.payment',
+      'revenue' => 'pages.revenue',
+      'orders' => 'pages.orders',
+      'seller-request' => 'pages.seller-request',
+      'product-create' => 'pages.product-create',
+      'product-list' => 'pages.product-list',
+      'rfq-create' => 'pages.rfq-create',
+      'rfq-list' => 'pages.rfq-list',
+      'quotation-list' => 'pages.quotation-list',
+      'admin' => 'pages.admin',
+      'seller' => 'pages.seller',
+      'support' => 'pages.support',
+      'support-workspace' => 'pages.support-workspace',
+    ];
+  @endphp
+
+  @if(isset($pageViews[$slug]) && view()->exists($pageViews[$slug]))
+    @include($pageViews[$slug])
+  @else
+    <h1>{{ get_the_title() }}</h1>
+    {!! the_content() !!}
+  @endif
 @endsection
